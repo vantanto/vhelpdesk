a0ac7cf90e5be90f56e3da8fc5c0c928fc1c6206
vantanto <56611021+vantanto@users.noreply.github.com>
2023-03-09 16:48:59 +0700

    add: email icon on reset password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -20,6 +20,12 @@
                             <div class="mb-4">
                                 <label for="email">Your Email</label>
                                 <div class="input-group">
+                                    <span class="input-group-text">
+                                        <svg class="icon icon-xs text-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                                        </svg>
+                                    </span>
                                     <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}" required autofocus>
                                 </div>
                                 <x-input-error :messages="$errors->get('email')" />
